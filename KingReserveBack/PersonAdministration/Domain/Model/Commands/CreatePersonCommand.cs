e018beb789fb6704df4c53b4af362c2dc0d8e374
JoaquinPedraza1 <u202218514@upc.edu.pe>
2024-11-03 20:21:08 -0500

    feat(person) Fixed PersonAdministration resources

---
+++ KingReserveBack/PersonAdministration/Domain/Model/Commands/CreatePersonCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace KingReserveBack.PersonAdministration.Domain.Model.Commands;
+
+public record CreatePersonCommand(
+    string Name,
+    int Age,
+    DateOnly Date,
+    string Country,
+    string City,
+    string District,
+    string Observations,
+    int RoomId
+);
